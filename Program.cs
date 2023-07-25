f3448220e92f3fb714a50f0ca4495b0788dae4cd
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-07-24 22:56:17 -0700

    :construction: Add Create, Read operations from Users

--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
+using FakeStore.Database;
+using FakeStore.Database.Models;
+using FakeStoreApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.UseFakeStoreDatabase(new FakeDatabaseConfigurator()
+{
+    UsersConfiguration = new UsersConfigurator()
+    {
+        MaxDefaultUsers = 10,
+        NullProbability = 0.2f
+    },
+    ProductsConfiguration = new ProductsConfigurator()
+    {
+        MaxPrice = 10000,
+        MinPrice = 10,
+        NullProbability = 0.2f
+    },
+    CategoriesConfiguration = new CategoriesConfigurator()
+    {
+        MaxCategories = 10,
+        NullProbability = 0.2f
+    }
+});
+
+builder.Services.AddScoped<IUsers, UserService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
