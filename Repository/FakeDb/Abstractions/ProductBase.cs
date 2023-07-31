23bb1c0e4aed2893e2d96851f40369453028614d
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-07-30 23:35:54 -0700

    Create ProductBase.cs

---
+++ Repository/FakeDb/Abstractions/ProductBase.cs
@@ -0,0 +1,18 @@
+﻿using FakeStore.Database.Models;
+
+namespace FakeStoreApi.Repository.FakeDb.Abstractions
+{
+    public abstract class ProductBase : IProducts
+    {
+        public abstract void EditProduct(int ProductId, string Category, string Name, string Description, float Price);
+        public abstract FakeProduct? GetProduct(int ProductId);
+        public abstract List<FakeProduct> GetProducts();
+    }
+
+    public interface IProducts
+    {
+        void EditProduct(int ProductId, string Category, string Name, string Description, float Price);
+        FakeProduct? GetProduct(int ProductId);
+        List<FakeProduct> GetProducts();
+    }
+}
