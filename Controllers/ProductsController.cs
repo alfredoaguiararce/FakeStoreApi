aa7e3ba19f42e9195a65b8887d7edbfb17338ac9
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-07-31 00:12:24 -0700

    :hammer: Controllers are already created and works
    [ADD] Products controller
    
    TODO: Create the valid methods in all controllers

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using FakeStoreApi.Repository.FakeDb.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeStoreApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        private readonly IProducts products;
+
+        public ProductsController(IProducts products)
+        {
+            this.products = products;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(products.GetProducts());
+        }
+    }
+}
